44dfe44dcbc7c3677c49110421447b217299b370
Tatu Leppämäki <43028964+Tadusko@users.noreply.github.com>
2020-08-16 12:55:20 +0300

    Manskun appron verkkokartta lisätty

---
+++ Mannerheimin_appro_verkkokartta/data/varrenjuottolat_3.js
@@ -0,0 +1,22 @@
+var json_varrenjuottolat_3 = {
+"type": "FeatureCollection",
+"name": "varrenjuottolat_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Baari": "Mullikka", "Järjestysluku": 3.0, "kuva_url": "images\/mullikka.png" }, "geometry": { "type": "Point", "coordinates": [ 24.9068404, 60.1931289 ] } },
+{ "type": "Feature", "properties": { "Baari": "William K", "Järjestysluku": 10.0, "kuva_url": "images\/william_k.png" }, "geometry": { "type": "Point", "coordinates": [ 24.9238001, 60.1834919 ] } },
+{ "type": "Feature", "properties": { "Baari": "Pub 99", "Järjestysluku": 12.0, "kuva_url": "images\/pub99.png" }, "geometry": { "type": "Point", "coordinates": [ 24.9267371, 60.18186 ] } },
+{ "type": "Feature", "properties": { "Baari": "Karhu Ministeri", "Järjestysluku": 7.0, "kuva_url": "images\/karhu_ministeri.png" }, "geometry": { "type": "Point", "coordinates": [ 24.917760538759836, 60.18931888020704 ] } },
+{ "type": "Feature", "properties": { "Baari": "Ravintola Mexi", "Järjestysluku": 5.0, "kuva_url": "images\/mexi.jpg" }, "geometry": { "type": "Point", "coordinates": [ 24.91659771231188, 60.190127155785149 ] } },
+{ "type": "Feature", "properties": { "Baari": "Ölhus Helsinki", "Järjestysluku": 6.0, "kuva_url": "images\/ölhus.png" }, "geometry": { "type": "Point", "coordinates": [ 24.915868623593607, 60.189323668584095 ] } },
+{ "type": "Feature", "properties": { "Baari": "Ravintola Viisi Penniä", "Järjestysluku": 4.0, "kuva_url": "images\/viisi_penniä.png" }, "geometry": { "type": "Point", "coordinates": [ 24.915014814032492, 60.190573450489275 ] } },
+{ "type": "Feature", "properties": { "Baari": "Bar 36", "Järjestysluku": 2.0, "kuva_url": "images\/bar36.png" }, "geometry": { "type": "Point", "coordinates": [ 24.8999173, 60.1952162 ] } },
+{ "type": "Feature", "properties": { "Baari": "LÄHTÖ: Felt Bay", "Järjestysluku": 1.0, "kuva_url": "images\/feltbay.png" }, "geometry": { "type": "Point", "coordinates": [ 24.9000614, 60.1975597 ] } },
+{ "type": "Feature", "properties": { "Baari": "St Urho's Pub", "Järjestysluku": 13.0, "kuva_url": "images\/st_urhos.png" }, "geometry": { "type": "Point", "coordinates": [ 24.93108931081969, 60.174328420904175 ] } },
+{ "type": "Feature", "properties": { "Baari": "Storyville", "Järjestysluku": 14.0, "kuva_url": "images\/storyville.png" }, "geometry": { "type": "Point", "coordinates": [ 24.932625178513728, 60.173760173970635 ] } },
+{ "type": "Feature", "properties": { "Baari": "Bar Fellows", "Järjestysluku": 11.0, "kuva_url": "images\/fellows.png" }, "geometry": { "type": "Point", "coordinates": [ 24.9255271, 60.1826215 ] } },
+{ "type": "Feature", "properties": { "Baari": "Korjaamo", "Järjestysluku": 9.0, "kuva_url": "images\/korjaamo.png" }, "geometry": { "type": "Point", "coordinates": [ 24.919796, 60.1842069 ] } },
+{ "type": "Feature", "properties": { "Baari": "MAALI: Marskin patsas", "Järjestysluku": 15.0, "kuva_url": "images\/mannerheim.jpg" }, "geometry": { "type": "Point", "coordinates": [ 24.936431714665645, 60.171627438124887 ] } },
+{ "type": "Feature", "properties": { "Baari": "Sports Bar Töölö", "Järjestysluku": 8.0, "kuva_url": "images\/spors_bar.png" }, "geometry": { "type": "Point", "coordinates": [ 24.9175873706332, 60.188165492381536 ] } }
+]
+}
